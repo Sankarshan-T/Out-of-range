7b7db14a6e81afdad4d2d0133a627f1f2c6d58d3
CoolCream <mahasankarshant@gmail.com>
2026-03-06 08:34:36 +0530

    some controls

--- Scripts/Level_Manager.gd
+++ Scripts/Level_Manager.gd
@@ -1,17 +1,46 @@
-extends Node
+extends Node2D
 
-func _on_start_pressed() -> void:
-	Dialogic.start("tolevel1")
-	await Dialogic.timeline_ended
-	navigate_to_level("res://Scenes/Levels/level_1.tscn")
+@export_group("Level Settings")
+@export var level_no: int
+@export var next_level: PackedScene 
+@export var level_timeline: String = ""
+
+@onready var level_label = find_child("LevelLabel", true, false)
+@onready var energy_label = find_child("EnergyLabel", true, false)
+
+func _ready() -> void:
+	if level_label:
+		level_label.text = "Level: " + str(level_no)
+	update_energy_ui()
+
+func _process(_delta: float) -> void:
+	update_energy_ui()
+
+func update_energy_ui() -> void:
+	var total_energies = get_tree().get_nodes_in_group("Energies").size()
+	if energy_label:
+		energy_label.text = str(total_energies) + " to collect"
+	
+	if total_energies <= 0 and not is_queued_for_deletion():
+		set_process(false) 
+		complete_level()
+
+func complete_level() -> void:
+	await get_tree().create_timer(1.0).timeout
+	
+	var player = get_tree().get_first_node_in_group("Player")
+	
+	if level_timeline != "":
+		if player: player.is_frozen = true
+		Dialogic.start(level_timeline)
+		await Dialogic.timeline_ended
+		if player: player.is_frozen = false
+	
+	if next_level:
+		get_tree().change_scene_to_packed(next_level)
+	else:
+		print("Mission Accomplished! (No next level set)")
 
 func navigate_to_level(path: String) -> void:
-	if path == "":
-		print("Error: No path provided to LevelManager!")
-		return
-		
+	if path == "": return
 	get_tree().call_deferred("change_scene_to_file", path)
-
-
-func _on_story_pressed() -> void:
-	Dialogic.start("story")
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,24 +5,22 @@ const JUMP_VELOCITY = -500.0
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var anim: AnimationPlayer = $AnimationPlayer
-@onready var canvas_modulate: CanvasModulate = $"../CanvasModulate"
 @onready var point_light_2d: PointLight2D = $PointLight2D
 
-@export var level_timeline: String = "" 
-@export var next_level: PackedScene 
-
+var canvas_modulate: CanvasModulate
 var is_frozen = false
+
+func _ready():
+	add_to_group("Player")
+	canvas_modulate = get_parent().find_child("CanvasModulate", true, false)
 
 func _physics_process(delta):
 	if is_frozen:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if not is_on_floor():
-			velocity += get_gravity() * delta
+		if not is_on_floor(): velocity += get_gravity() * delta
 		move_and_slide()
 		anim.play("Idle")
 		return
-		
-	var total_energies = get_tree().get_nodes_in_group("Energies").size()
 		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -31,53 +29,19 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	var direction = Input.get_axis("ui_left", "ui_right")
-	
 	if direction:
 		velocity.x = direction * SPEED
 		sprite.flip_h = (direction < 0)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta * 10)
 	
-	var label = get_tree().current_scene.find_child("Label", true, false)
-
-	if label:
-		label.text = str(total_energies) + " to collect"
-	else:
-		print("Warning: Could not find Label")
-		
 	move_and_slide()
-	update_animations(direction)
+	update_animations()
 
-func update_animations(direction):
-	if velocity.x != 0:
-		anim.play("Run")
-	else:
-		anim.play("Idle")
-		
+func update_animations():
+	anim.play("Run" if velocity.x != 0 else "Idle")
+
 func _on_energy_collected():
-	var light_step = 0.05
-	
-	var new_r = clamp(canvas_modulate.color.r + light_step, 0.0, 1.0)
-	var new_g = clamp(canvas_modulate.color.g + light_step, 0.0, 1.0)
-	var new_b = clamp(canvas_modulate.color.b + light_step, 0.0, 1.0)
-	
-	canvas_modulate.color = Color(new_r, new_g, new_b)
+	if canvas_modulate:
+		canvas_modulate.color = canvas_modulate.color.lightened(0.05)
 	point_light_2d.scale *= 1.1
-	
-	if get_tree().get_nodes_in_group("Energies").size() < 1:
-		await get_tree().create_timer(1.0).timeout
-		play_end_of_level()
-
-func play_end_of_level():
-	if level_timeline != "":
-		is_frozen = true
-		
-		Dialogic.start(level_timeline)
-		await Dialogic.timeline_ended
-		
-		is_frozen = false
-	
-	if next_level:
-		get_tree().change_scene_to_packed(next_level)
-	else:
-		print("drag next level into the Inspector!")
